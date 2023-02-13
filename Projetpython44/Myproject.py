b75c207107fe76b0a69ef2492ac14994dabf03a0
Jessica4488 <user@gcp-pcp-powell.emse2000.local>
2023-02-13 09:55:49 +0100

    First 09h55

---
+++ Projetpython44/Myproject.py
@@ -0,0 +1,2 @@
+# https://www.youtube.com/watch?v=ns7cmSaiA9E&list=PLgzUSl6Ph6jvdEV4XSMPHekmXzuFoSjo1&index=1 App web moderne avec Python (Docstring)
+
--- Testkeithley/test.py
+++ Testkeithley/test.py
@@ -1,1 +1,10 @@
-print("Hi !")
+from PyQt5 import QtWidgets
+from keithley2600 import Keithley2600
+from keithleygui import KeithleyGuiApp
+
+app = QtWidgets.QApplication([])
+
+keithley = Keithley2600('TCPIP::169.254.0.1::INSTR')
+keithley_gui = KeithleyGuiApp(keithley)
+keithley_gui.show()
+app.exec()
