56ed05e6bbc9c033bbff7d7d7580122a223e897b
Jessica.Jouvencel <j.jouvencel@live.fr>
2023-03-13 09:03:29 +0100

    Update 13/03

--- Projetpython44/readme.md
+++ Projetpython44/readme.md
@@ -13,11 +13,12 @@ Proof of concept(POC)
     - collection (Foreignkey)
 
 ## Fonctionnalités
-- Ajouter une collection
+[X] Ajouter une collection
 - Supprimer une collection
 - Renommer une collection
-- Empêcher l'ajout d'une collection en doublon 
+[X] Empêcher l'ajout d'une collection en doublon 
 - Ajouter une tâche (reliée à une collection) 
 - Supprimer une tâche 
 - Afficher les tâches d'une collection 
+- Créer une liste par défaut
 
--- Projetpython44/tasks/views.py
+++ Projetpython44/tasks/views.py
@@ -7,12 +7,21 @@ from tasks.models import Collection
 
 def index(request):
     context = {}
-    context["collections"] = Collection.objects.order_by("name")
+
+    collection = Collection.get_default_collection()
+    context["collections"] = Collection.objects.order_by("slug")
 
     return render(request, 'tasks/index.html', context=context)
 
 # on créer la vue et l'import
 def add_collection(request):
     collection_name = escape(request.POST.get("collection-name"))
-    Collection.objects.get_or_create(name=collection_name)
-    return redirect('home')
+    collection, created = Collection.objects.get_or_create(name=collection_name)
+    if not created:
+        return HttpResponse("La collection existe déja.", status=409)
+
+    return HttpResponse(f'<h2>{collection_name}</h2>')
+
+
+def add_task(request):
+    return HttpResponse("")
